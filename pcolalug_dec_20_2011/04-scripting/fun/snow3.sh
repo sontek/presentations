adc814ecf4c8fc339e694515748f35feb5aa858c
John Anderson <sontek@gmail.com>
2011-12-21 04:09:10 -0600

    added a better working snow script

---
+++ pcolalug_dec_20_2011/04-scripting/fun/snow3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+LINES=$(tput lines)
+COLUMNS=$(tput cols)
+
+clear
+
+declare -A snowflakes
+declare -A lastflakes
+
+function move_flake() {
+    i="$1"
+
+    if [ "${snowflakes[$i]}" = "" ] || [ "${snowflakes[$i]}" = "$LINES" ]; then
+        snowflakes[$i]=0
+    else
+        if [ "${lastflakes[$i]}" != "" ]; then
+            printf "\033[%s;%sH \033[0;0H " ${lastflakes[$i]} $i
+        fi
+    fi
+
+    printf "\033[%s;%sH*\033[0;0H" ${snowflakes[$i]} $i
+
+    lastflakes[$i]=${snowflakes[$i]}
+    snowflakes[$i]=$((${snowflakes[$i]}+1))
+
+}
+
+while :
+do
+    i=$(($RANDOM % $COLUMNS))
+
+    move_flake $i
+
+    for x in "${!lastflakes[@]}"
+    do
+        move_flake "$x"
+    done
+
+    sleep 0.1
+done
+
